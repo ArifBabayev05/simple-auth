7852bf9096feab1a182e364486b350222170fbcf
Arif Babayev <arifrb@code.edu.az>
2024-06-27 21:55:57 +0400

    add credentials to api

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ app.use(express.json());
 const corsOptions = {
     origin: ['http://localhost:5173', 'https://igame.live'],
     optionsSuccessStatus: 200,
+    credentials:true,  
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
 };
