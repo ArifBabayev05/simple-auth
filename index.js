193a402b8f4909d4dc4e106aca0da5abf846b97f
Arif Babayev <arifrb@code.edu.az>
2024-06-27 21:24:27 +0400

    cors config changed again

--- index.js
+++ index.js
@@ -11,7 +11,9 @@ app.use(cors())
 
 const corsOptions = {
     origin: ['http://localhost:5173', 'https://igame.live'],
-    optionsSuccessStatus: 200
+    optionsSuccessStatus: 200,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
   };
   
 
